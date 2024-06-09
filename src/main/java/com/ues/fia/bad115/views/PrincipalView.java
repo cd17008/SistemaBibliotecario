c57078dbcc1f892c387508d1c92e0d033dadf2aa
Nathaly-15 <rm15118@ues.edu.sv>
2024-06-09 07:49:12 -0600

    Menú principal con fondo

--- src/main/java/com/ues/fia/bad115/views/PrincipalView.java
+++ src/main/java/com/ues/fia/bad115/views/PrincipalView.java
@@ -2,12 +2,12 @@ package com.ues.fia.bad115.views;
 
 import com.ues.fia.bad115.clase.Usuario;
 import com.ues.fia.bad115.component.NavBar;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinSession;
@@ -27,30 +27,34 @@ public class PrincipalView extends VerticalLayout {
 
     public PrincipalView(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
-        setClassName("login");
-        Image background = new Image("images/principal.png", "Biblioteca Central");
-        background.setWidth("100%");
-        background.setHeight("auto");
+        setClassName("principal-view");
+
         try {
             Usuario usuario = usuarioService.getUsuarioByEmail(email);
             nombre = " " + usuario.getNombre();
         } catch (Exception e) {
             nombre = "";
         }
+
         H1 title = new H1("¡Bienvenido" + nombre + "!");
         title.getStyle().setColor("white");
 
-        H2 subtitulo = new H2("¿Qué deseas consultar?");
+        H3 subtitulo = new H3("¿Qué deseas consultar?");
         subtitulo.getStyle().setColor("white");
 
         // Botones
-        Button usuariosButton = new Button("USUARIOS");
-        Button recursosButton = new Button("RECURSOS");
-        Button prestamosButton = new Button("PRESTAMOS");
-        Button morasButton = new Button("MORAS");
-        Button reporButton = new Button("REPORTES");
+        Button usuariosButton = new Button("USUARIOS", new Icon(VaadinIcon.USERS));
+        Button recursosButton = new Button("RECURSOS", new Icon(VaadinIcon.BOOK));
+        Button prestamosButton = new Button("PRESTAMOS", new Icon(VaadinIcon.ARROW_RIGHT));
+        Button reporButton = new Button("REPORTES", new Icon(VaadinIcon.CHART));
+        //Button morasButton = new Button("MORAS");
 
         // Estilo para los botones
+        usuariosButton.getStyle().set("font-size", "2em");
+        recursosButton.getStyle().set("font-size", "2em");
+        prestamosButton.getStyle().set("font-size", "2em");
+        reporButton.getStyle().set("font-size", "2em");
+
         usuariosButton.addClassName("main-button");
         usuariosButton.addClickListener(e -> {
             usuariosButton.getUI().ifPresent(ui -> ui.navigate("usuarios"));
@@ -63,22 +67,27 @@ public class PrincipalView extends VerticalLayout {
         prestamosButton.addClickListener(e -> {
             prestamosButton.getUI().ifPresent(ui -> ui.navigate("prestamos"));
         });
-        morasButton.addClassName("main-button");
+        /*morasButton.addClassName("main-button");
         morasButton.addClickListener(e -> {
             morasButton.getUI().ifPresent(ui -> ui.navigate("moras"));
-        });
+        });*/
         reporButton.addClassName("main-button");
         reporButton.addClickListener(e -> {
             reporButton.getUI().ifPresent(ui -> ui.navigate("reportes"));
         });
 
         // Layout de botones
-        HorizontalLayout buttonsLayout = new HorizontalLayout(usuariosButton, recursosButton, prestamosButton,
+        /*HorizontalLayout buttonsLayout = new HorizontalLayout(usuariosButton, recursosButton, prestamosButton,
                 morasButton, reporButton);
         buttonsLayout.setJustifyContentMode(JustifyContentMode.CENTER);
-        buttonsLayout.addClassName("main-buttons");
+        buttonsLayout.addClassName("main-buttons");*/
+        HorizontalLayout firstRow = new HorizontalLayout(usuariosButton, recursosButton);
+        HorizontalLayout secondRow = new HorizontalLayout(prestamosButton, reporButton);
 
-        add(navegacion, title, subtitulo, buttonsLayout);
+        firstRow.setJustifyContentMode(JustifyContentMode.CENTER);
+        secondRow.setJustifyContentMode(JustifyContentMode.CENTER);
+
+        add(navegacion, title, subtitulo, firstRow, secondRow);
     }
 
 }
